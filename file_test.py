1c323867d99987ff9c37a92cc3327ce15da15751
0kazou <pierre-henri.louf@wanadoo.fr>
2025-02-23 09:33:16 +0100

    comments before in filte_test.py

--- file_test.py
+++ file_test.py
@@ -1,25 +1,24 @@
 import pytest
 
 
-def test_calc_addition():
 # Fonction test du résultat de 2+4
+def test_calc_addition():
     output = 2+4
     assert output == 6
 
-
-def test_calc_substraction():
 # Fonction test du résultat de 2-4
+def test_calc_substraction():
     output = 2-4
     assert output == -2
 
 
-def test_calc_multiply():
 # Fonction test du résultat de 2*4
+def test_calc_multiply():
     output = 2*4
     assert output == 8
 
 
-def test_coucou():
 # Fonction test si la résultat renvoie 'hello'
-    output='hello'
+def test_coucou():
+    output = 'hello'
     assert output == 'hello'
